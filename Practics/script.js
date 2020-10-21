a41d7677b1eb0767bfefec1219df132fdd67c7fb
Alex <asxp@mail.ru>
2020-10-21 16:28:55 +0300

    коммит от 21 октября

--- Practics/script.js
+++ Practics/script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','')
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -6,14 +6,28 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
+};
+
+for (let i=0; i<2; i++) {
+    const a = prompt('Ответьте на 2 вопроса. Один из последних просмотренных фильмов?',''),
+          b = prompt('На сколько оцените его?','');
+
+    if (a != null && a.length < 50 && a != '') {
+        personalMovieDB.movies[a] = b;        
+    }else{
+        console.log('err');
+        i--;
+    }
+
+    
+  
 }
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько оцените его?',''),  
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?','')  
+// const a = prompt('Один из последних просмотренных фильмов?',''),
+//       b = prompt('На сколько оцените его?',''),  
+//       c = prompt('Один из последних просмотренных фильмов?',''),
+//       d = prompt('На сколько оцените его?','')  
 
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
 
-console.log(personalMovieDB)
+
+console.log(personalMovieDB);
